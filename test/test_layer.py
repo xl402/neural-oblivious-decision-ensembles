ac783c7f948ba43bd1c241b6569d2e3f97c33b0e
xl402 <xl402@cam.ac.uk>
2020-12-12 16:56:03 +0000

    test multiple cases for binary look up

--- test/test_layer.py
+++ test/test_layer.py
@@ -4,17 +4,19 @@ import pytest
 
 from node.networks.layer import get_binary_lookup_table
 
+TREE_DEPTHS = [1, 2, 3]
 
-def test_binary_lookup_table_last_dimension_is_complementary():
-    depth = 3
+
+@pytest.mark.parametrize('depth', TREE_DEPTHS)
+def test_binary_lookup_table_last_dimension_is_complementary(depth):
     lut = get_binary_lookup_table(depth).numpy()
     out = np.sum(lut, axis=-1)
     expected_shape = (depth, 2**depth)
     assert np.all(np.ones(expected_shape) == out)
 
 
-def test_binary_lookup_table_enumerates_over_all_binary_combination_space():
-    depth = 3
+@pytest.mark.parametrize('depth', TREE_DEPTHS)
+def test_binary_lookup_table_enumerates_over_all_binary_combination_space(depth):
     lut = get_binary_lookup_table(depth).numpy()
     lut = lut[:, :, 0]
     expected_shape = (depth, 2**depth)
